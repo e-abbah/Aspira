7e5d0b45fee698d48bffbed7f7519e63d36d2050
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-08-15 18:56:27 +0100

    feat: import OpportunitiesHub component and define route for opportunities page

--- aspira-app/src/App.tsx
+++ aspira-app/src/App.tsx
@@ -9,6 +9,7 @@ import DashboardPage from "./pages/Dashboard";
 import Journey from './pages/Journey';
 import Workspace from "./pages/Workspace";
 import { LearningHubPage } from './pages/LearningHubPage';
+import OpportunitiesHub from "./pages/OpportunitesHub.tsx";
 
 
 // ...
@@ -27,7 +28,7 @@ export default function App() {
          <Route path="/journey" element={<Journey/>} />
          <Route path="/workspace" element= {<Workspace />} />
          <Route path="/learning-hub" element={<LearningHubPage />} />
-         {/* </Routes><Route path="/opportunities" element={</>} /> */}
+         <Route path="/opportunities" element={<OpportunitiesHub/>} /> 
       </Routes>
     </BrowserRouter>
   );
