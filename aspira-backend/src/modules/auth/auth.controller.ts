cd631dc7ab79aa1510789e05af89fb613e0ce08c
emmanuel <emmanuel.abbah@pau.edu.ng>
2026-08-19 01:37:50 +0100

    feat: implement JWT token generation and verification utilities

--- aspira-backend/src/modules/auth/auth.controller.ts
+++ aspira-backend/src/modules/auth/auth.controller.ts
@@ -3,27 +3,38 @@ import { signupSchema } from "./auth.schema";
 import { signupUser } from "./auth.service";
 
 export const signup = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  try {
-    const parsed = signupSchema.safeParse(req.body);
+    try {
+        const parsed = signupSchema.safeParse(req.body);
 
-    if (!parsed.success) {
-      return res.status(400).json({
-        message: "Validation failed",
-        errors: parsed.error.issues,
-      });
+        // if (!parsed.success) {
+        //   return res.status(400).json({
+        //     message: "Validation failed",
+        //     errors: parsed.error.issues,
+        //   });
+        // }
+        if (!parsed.success) {
+            const errors = parsed.error.issues.map((issue) => ({
+                field: issue.path.join("."),
+                message: issue.message,
+            }));
+
+            return res.status(400).json({
+                message: "Validation failed",
+                errors,
+            });
+        }
+
+        const user = await signupUser(parsed.data);
+
+        return res.status(201).json({
+            message: "Account created successfully",
+            user,
+        });
+    } catch (err) {
+        next(err);
     }
-
-    const user = await signupUser(parsed.data);
-
-    return res.status(201).json({
-      message: "Account created successfully",
-      user,
-    });
-  } catch (err) {
-    next(err);
-  }
 };
---
+++ aspira-backend/src/utils/token.ts
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+const ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_SECRET as string;
+const REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_SECRET as string;
+
+const ACCESS_TOKEN_EXPIRY = "10m";
+const REFRESH_TOKEN_EXPIRY = "2d";
+
+export interface TokenPayload{
+    userId: string
+}
+
+export const generateAccessToken = (payload: TokenPayload): string => {
+    return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+        expiresIn: ACCESS_TOKEN_EXPIRY
+    });
+}
+
+export const generateRefreshToken = (payload: TokenPayload): string => {
+    return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+        expiresIn: REFRESH_TOKEN_EXPIRY
+    });
+}
+
+export const verifyAccessToken = (token: string): TokenPayload => {
+  return jwt.verify(token, ACCESS_TOKEN_SECRET) as TokenPayload;
+};
+
+export const verifyRefreshToken = (token: string): TokenPayload => {
+  return jwt.verify(token, REFRESH_TOKEN_SECRET) as TokenPayload;
+};
